171f4d627d8ad64ac2028b81a072b784a9d89eef
Michael Caunce <michaelcaunce@hotmail.co.uk>
2020-09-30 10:52:59 +0100

    Complete redesign to the hero, colors and new content added

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,53 +1,51 @@
 // Make a variable for the portfolio container = class portfolio-items. portfolio-items contains all portfolio elements.
-const portfolioContainer = document.querySelector('.portfolio__items')
+const portfolioContainer = document.querySelector(".portfolio__items")
 
 // addEventListener listening for a click,
-portfolioContainer.addEventListener('click', ev => {
-
+portfolioContainer.addEventListener("click", ev => {
   // ev.preventDefault()
 
   // Variable for turning on the modal in relation to the link clicked.
   // When the target is the closest class of portfolio-link
-  const modalToggle = ev.target.closest('.portfolio-link')
+  const modalToggle = ev.target.closest(".portfolio-link")
 
-  if(modalToggle) ev.preventDefault()
-  
+  if (modalToggle) ev.preventDefault()
+
   // If a click is not a portfolio-link, end script.
-  if (! modalToggle) return
+  if (!modalToggle) return
 
   // Variable modal is equal to the parent / next sibling of modalToggle. The parent being the figcaption, next sibling being the class portfolio-modal (see index.html)
   const modal = modalToggle.parentNode.nextElementSibling
   // Variable equal to class modal-close
-  const closeButton = modal.querySelector('.modal__section--button')
+  const closeButton = modal.querySelector(".modal__section--button")
 
   // modalOpen is equal to a function to add the open class and add an inline style for animation, modalIn (see _modal.scss)
   const modalOpen = _ => {
     // Open the modal by adding class open
-    modal.classList.add('open')
-    modal.style.animation = 'modalIn 1000ms forwards'
+    modal.classList.add("open")
+    modal.style.animation = "modalIn 1000ms forwards"
     // Remove back scroll
-    document.body.style.overflowY = 'hidden'
+    document.body.style.overflowY = "hidden"
   }
 
   // modalClose is equal to a function to remove class open and is called in function closeButton
   const modalClose = _ => {
     // Close the modal by removing class, open
-    modal.classList.remove('open')
+    modal.classList.remove("open")
     // Remove the eventListener to the modal can open again after closing
-    modal.removeEventListener('animationend', modalClose)
+    modal.removeEventListener("animationend", modalClose)
     // Add back scroll
-    document.body.style.overflowY = 'scroll'
+    document.body.style.overflowY = "scroll"
   }
 
   // closeButton is equal to a function to fade the modal out and look for the end of the animation followed by running a function.
-  closeButton.addEventListener('click', _=> {
-    modal.style.animation = 'modalOut 1000ms forwards'
-    modal.addEventListener('animationend', modalClose)
+  closeButton.addEventListener("click", _ => {
+    modal.style.animation = "modalOut 1000ms forwards"
+    modal.addEventListener("animationend", modalClose)
     // Add back scroll
-    document.body.style.overflowY = 'scroll'
+    document.body.style.overflowY = "scroll"
   })
 
   // Call the function
   modalOpen()
-
 })
---
+++ js/scroll.js
@@ -0,0 +1,42 @@
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "150px",
+  duration: 2000,
+  reset: false
+})
+
+/*SCROLL HOME*/
+sr.reveal(".header--logo", {})
+sr.reveal(".navigation ul", { delay: 200 })
+sr.reveal(".hero--img", { origin: "right", scale: 0.15, delay: 600 })
+sr.reveal(".hero__content--title", { origin: "left", delay: 600 })
+sr.reveal(".hero__content--sub--small", { origin: "left", delay: 800 })
+sr.reveal(".hero__content--sub--XL", { origin: "left", delay: 800 })
+sr.reveal(".hero__footer", { origin: "left", delay: 1000 })
+sr.reveal(".hero__content--text", { origin: "right", delay: 1200 })
+sr.reveal(".hero--text", { origin: "right", delay: 1200 })
+
+/*SCROLL ABOUT*/
+sr.reveal(".about__section--title", { origin: "left", delay: 300 })
+sr.reveal(".about__section--text", { origin: "bottom", delay: 400 })
+sr.reveal(".about__section--img", { origin: "bottom", delay: 700 })
+
+/*SCROLL SKILLS*/
+sr.reveal(".skills--title", {})
+sr.reveal(".skills__list--item", { distance: "20px", origin: "left", delay: 90, interval: 100 })
+
+/*SCROLL QUOTE*/
+sr.reveal(".quote_1", { delay: 400 })
+sr.reveal(".quote_2", { delay: 800 })
+
+/*SCROLL PORTFOLIO*/
+sr.reveal(".portfolio__section--title", { origin: "left", delay: 300 })
+sr.reveal(".portfolio__section--filter--buttons--list", { distance: "50px", origin: "bottom", delay: 90, interval: 100 })
+// sr.reveal(".portfolio__items--item", { distance: "10px", origin: "bottom", delay: 500, interval: 300, scale: 0.15 })
+
+/*SCROLL CONTACT*/
+sr.reveal(".contact__container", { delay: 2000, scale: 0.15 })
+sr.reveal(".contact--title", { origin: "left", delay: 1900 })
+sr.reveal(".contact__container--img", { delay: 3000 })
+sr.reveal(".contact__container--box--icons li", { distance: "20px", origin: "left", delay: 2400, interval: 300 })
